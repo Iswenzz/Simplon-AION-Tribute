6a7a42dcfc276d2a3dd00008fc9e5808dc6c1d60
Iswenzz <suxlolz1528@gmail.com>
2020-10-02 15:58:05 +0200

    Paper: fantasy style section with a preview image and a paper

---
+++ src/components/Paper/Paper.tsx
@@ -0,0 +1,41 @@
+import React, {PureComponent} from "react";
+import {Container, Grid, Typography} from "@material-ui/core";
+import "./Paper.scss";
+
+export interface PaperProps
+{
+	title?: string,
+	desc?: string,
+	image?: string,
+	className?: string,
+	previewStyle?: React.CSSProperties,
+	paperStyle?: React.CSSProperties
+}
+
+export class Paper extends PureComponent<PaperProps>
+{
+	public render(): JSX.Element
+	{
+		return (
+			<section className={this.props.className}>
+				<div className="paper">
+					<Grid container direction={"column"} justify={"space-evenly"} alignItems={"center"} className={"paper-container"} style={this.props.paperStyle}>
+						<Typography align={"center"} component={"h2"} variant={"h2"}>
+							{this.props.title}
+						</Typography>
+						<Container>
+							<Typography component={"p"} paragraph>
+								{this.props.desc}
+							</Typography>
+						</Container>
+						<section className={"paper-preview"} style={this.props.previewStyle}>
+							<img src={this.props.image} alt={"Presentation Paper"} />
+						</section>
+					</Grid>
+				</div>
+			</section>
+		);
+	}
+}
+
+export default Paper;
